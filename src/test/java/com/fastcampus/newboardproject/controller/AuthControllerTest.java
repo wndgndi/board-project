67e2c827479b18f1c50d4d77d0521f98e918fd9f
wndgndi <wndgn456@gmail.com>
2023-01-27 20:20:07 +0900

    #28 - 예제 문자 삭제, 로그인 테스트 문제 해결
    부트스트랩 예제 코드를 사용하면서 복사된 예제 문자열 삭제
    `/login` 페이지는 직접 만든 컨트롤러가 아니고
    스프링 시큐리티가 해주는 작업이므로
    컨트롤러 테스트에서 읽어야 할 컨트롤러 빈이 없음
    이를 `Void.class`로 표현

--- src/test/java/com/fastcampus/newboardproject/controller/AuthControllerTest.java
+++ src/test/java/com/fastcampus/newboardproject/controller/AuthControllerTest.java
@@ -12,11 +12,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @DisplayName("View 컨트롤러 - 인증")
 @Import(SecurityConfig.class)
-@WebMvcTest
+@WebMvcTest(void.class)
 public class AuthControllerTest {
 
     private final MockMvc mvc;
@@ -32,6 +33,7 @@ public class AuthControllerTest {
         // When & Then
         mvc.perform(get("/login"))
             .andExpect(status().isOk())
-            .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+            .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+            .andDo(MockMvcResultHandlers.print());
     }
 }
