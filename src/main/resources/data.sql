22349ce79f2c5e423d03119383d2891dc2961319
wndgndi <wndgn456@gmail.com>
2023-03-22 15:12:57 +0900

    #35 - 패스워드 인코더가 인식할 수 있게 패스워드 정보 수정
    앞머리에 `{encoding-info}`와 같이 암호화 정보를 넣어줘야
    패스워드 인코더가 읽을 수 있다.
    이에 패스워드 데이터를 알맞게 수정함

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,11 +2,11 @@
 -- TODO: 테스트용이지만 비밀번호가 노출된 데이터 세팅. 개선하는 것이 좋을 지 고민해 보자.
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by,
                           modified_at, modified_by)
-values ('wndgn', 'asdf1234', 'wndgn', 'wndgn@mail.com', 'I am wndgn.', now(), 'wndgn', now(), 'wndgn')
+values ('wndgn', '{noop}asdf1234', 'wndgn', 'wndgn@mail.com', 'I am wndgn.', now(), 'wndgn', now(), 'wndgn')
 ;
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by,
                           modified_at, modified_by)
-values ('wndgn2', 'asdf1234', 'wndgn2', 'wndgn2@mail.com', 'I am wndgn2.', now(), 'wndgn2', now(), 'wndgn2')
+values ('wndgn2', '{noop}asdf1234', 'wndgn2', 'wndgn2@mail.com', 'I am wndgn2.', now(), 'wndgn2', now(), 'wndgn2')
 ;
 
 -- 123 게시글
